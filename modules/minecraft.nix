b4a3223f91b929d055fe96125ee2bc3b56cc4e5e
Ezra Wolf <wolfez@mail.uc.edu>
2025-11-01 18:07:50 -0400

    Added configs

--- modules/minecraft.nix
+++ modules/minecraft.nix
@@ -9,7 +9,7 @@
 }: let
   modpack = pkgs.fetchPackwizModpack {
     url = "https://raw.githubusercontent.com/EzraWolf/dotfiles/refs/heads/T2/minecraft/test/pack.toml";
-    packHash = "sha256-H+E3YzmWy/ogfY9xWT8RJhr6KXkPgOU3tQxW5XTWtXo=";
+    packHash = "sha256-Hb9FPwORyfmM+8jd5a2i1czVPyYMsvu9i0y4d/XqmDQ=";
   };
 in {
   imports = [inputs.nix-minecraft.nixosModules.minecraft-servers];
@@ -21,52 +21,6 @@ in {
     openFirewall = true;
 
     servers = {
-      french-vanilla = {
-        enable = true;
-        autoStart = true;
-        package = pkgs.fabricServers.fabric-1_20_1;
-
-        serverProperties = {
-          server-port = 25565;
-          white-list = true;
-          max-players = 16;
-          motd = "Bruh";
-
-          gamemode = "survival";
-          difficulty = "normal";
-
-          view-distance = 10;
-          simulation-distance = 10;
-        };
-
-        whitelist = {
-          schpinkledorf = "bbc70679-5257-483b-83d0-0bf1ebac4b4d";
-          #  CHIEF__BEER
-          #  josephus
-          #  nxndr
-          #  ServinVirgin (?)
-        };
-
-        operators = {
-          schpinkledorf = {
-            uuid = "bbc70679-5257-483b-83d0-0bf1ebac4b4d";
-            level = 4; # Owner level
-            bypassesPlayerLimit = true;
-          };
-        };
-
-        # Fetch local modpack
-        symlinks = {
-          "mods" = "${modpack}/mods";
-        };
-
-        #files = {
-        #  "config" = "${modpack}/config";
-          #"options.txt" = "${modpack}/options.txt";
-        #};
-
-        jvmOpts = "-Xms4G -Xmx4G -XX:+UseG1GC";
-      };
 
       vanilla = {
         enable = true;
